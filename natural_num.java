a68a06874598621f687f369c16596c633ab814c8
anshu887225 <129595811+anshu887225@users.noreply.github.com>
2023-08-12 11:48:17 +0530

    Update natural_num.java

--- natural_num.java
+++ natural_num.java
@@ -13,3 +13,24 @@
 
   }
 }
+
+
+// sum of n natural number
+int n, add = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the value of n");
+    n = sc.nextInt();
+
+    int i = 1;
+    while (i <= n) {
+      add = add + i;
+      System.out.println(" sum of natural number" + " " + add);
+      i = i + 1;
+    }
+
+
+output
+ 1
+ 3
+ 6
+ 10
