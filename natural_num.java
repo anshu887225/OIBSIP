5fba4c6c5929d33725c9ea749db5c4d4020a49d7
anshu887225 <129595811+anshu887225@users.noreply.github.com>
2023-08-12 11:35:54 +0530

    Create natural_num.java

---
+++ natural_num.java
@@ -0,0 +1,15 @@
+ public static void main(String[] args) {
+
+    int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the value of n");
+    n = sc.nextInt();
+
+    int i = 1;
+    while (i <= n) {
+      System.out.println("natural number" + " " + i);
+      i = i + 1;
+    }
+
+  }
+}
